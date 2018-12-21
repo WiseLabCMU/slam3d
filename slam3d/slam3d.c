21b3723da7cee6fde641539535b9884fa173d2bf
John Miller <jmiller4@andrew.cmu.edu>
2018-12-21 16:44:22 -0500

    write out files

--- slam3d/slam3d.c
+++ slam3d/slam3d.c
@@ -11,9 +11,10 @@
 
 #include "particlefilter/particleFilter.h"
 
-#define VIO_FILE    "/Users/johnmiller/Documents/MATLAB/mag_fld_matlab/BuddySLAM/data_CIC/1515283298.129726_vio.csv"
-#define UWB_FILE    "/Users/johnmiller/Documents/MATLAB/mag_fld_matlab/BuddySLAM/data_CIC/1515283298.129726_uwb_range.csv"
-#define OUT_FILE    "/Users/johnmiller/Desktop/test.csv"
+#define VIO_FILE        "/Users/johnmiller/Documents/MATLAB/mag_fld_matlab/BuddySLAM/data_CIC/1515283298.129726_vio.csv"
+#define UWB_FILE        "/Users/johnmiller/Documents/MATLAB/mag_fld_matlab/BuddySLAM/data_CIC/1515283298.129726_uwb_range.csv"
+#define TAG_OUT_FILE    "/Users/johnmiller/Desktop/tag.csv"
+#define BCN_OUT_FILE    "/Users/johnmiller/Desktop/bcn.csv"
 #define LINE_LEN    (1024)
 
 #define NUM_BCNS    (12)
@@ -21,22 +22,27 @@
 
 static uint8_t _getVio(FILE* vioFile, float* t, float* x, float* y, float* z);
 static uint8_t _getUwb(FILE* uwbFile, float* t, uint8_t* b, float* r);
+static void _writeTagLoc(FILE* outFile, float t, float x, float y, float z, float theta);
+static void _writeBcnLoc(FILE* outFile, uint8_t b, float x, float y, float z);
 
 int main()
 {
     FILE* vioFile;
     FILE* uwbFile;
-    FILE* outFile;
+    FILE* tagOutFile;
+    FILE* bcnOutFile;
     float vioT, vioX, vioY, vioZ;
     float uwbT, uwbR;
     uint8_t uwbB;
+    float outT, outX, outY, outZ, outTheta;
 	particleFilter_t particleFilter;
     bcn_t bcns[NUM_BCNS];
     uint8_t haveVio, haveUwb;
     
     vioFile = fopen(VIO_FILE, "r");
     uwbFile = fopen(UWB_FILE, "r");
-    outFile = fopen(OUT_FILE, "w");
+    tagOutFile = fopen(TAG_OUT_FILE, "w");
+    bcnOutFile = fopen(BCN_OUT_FILE, "w");
 	particleFilter_init(&particleFilter);
     
     haveVio = _getVio(vioFile, &vioT, &vioX, &vioY, &vioZ);
@@ -46,6 +52,8 @@ int main()
         if (haveVio && (!haveUwb || vioT < uwbT))
         {
             particleFilter_depositVio(&particleFilter, vioT, vioX, vioY, vioZ, 0.0f);
+            particleFilter_getTagLoc(&particleFilter, &outT, &outX, &outY, &outZ, &outTheta);
+            _writeTagLoc(tagOutFile, outT, outX, outY, outZ, outTheta);
             haveVio = _getVio(vioFile, &vioT, &vioX, &vioY, &vioZ);
         }
         else if (haveUwb)
@@ -54,10 +62,16 @@ int main()
             haveUwb = _getUwb(uwbFile, &uwbT, &uwbB, &uwbR);
         }
     }
+    for (uwbB = 0; uwbB < NUM_BCNS; ++uwbB)
+    {
+        particleFilter_getBcnLoc(&particleFilter, &bcns[uwbB], &outT, &outX, &outY, &outZ);
+        _writeBcnLoc(bcnOutFile, uwbB, outX, outY, outZ);
+    }
     
     fclose(vioFile);
     fclose(uwbFile);
-    fclose(outFile);
+    fclose(tagOutFile);
+    fclose(bcnOutFile);
     
 	return 0;
 }
@@ -91,4 +105,14 @@ static uint8_t _getUwb(FILE* uwbFile, float* t, uint8_t* b, float* r)
     *b = strtok(NULL, ",")[0] - 'a';
     *r = (float)atof(strtok(NULL, ",\n"));
     return 1;
+}
+
+static void _writeTagLoc(FILE* outFile, float t, float x, float y, float z, float theta)
+{
+    fprintf(outFile, "%f,%f,%f,%f,%f\n", t, x, y, z, theta);
+}
+
+static void _writeBcnLoc(FILE* outFile, uint8_t b, float x, float y, float z)
+{
+    fprintf(outFile, "%hhu,%f,%f,%f\n", b, x, y, z);
 }
