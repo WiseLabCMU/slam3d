ce8efc59e8c84a4a0eda4b1245df0c7b30cd94a6
John Miller <jmiller4@andrew.cmu.edu>
2019-01-11 16:29:19 -0500

    [update] switch to Arulampalam sampling

--- slam3d/particlefilter/pfResample.c
+++ slam3d/particlefilter/pfResample.c
@@ -21,16 +21,13 @@
 #define HXYZ                (0.1f)
 
 static void _resampleBcn(bcn_t* bcn, const tag_t* tag, float range, float stdRange, uint8_t force);
-static void _getRandCdf(float* buf, int count, float s);
-static int _floatCmp(const void* a, const void* b);
 
 void pfResample_resample(tag_t* tag, bcn_t* bcn, bcn_t* firstBcn, float range, float stdRange)
 {
     int i, j;
     tagParticle_t* tp;
-    float w, s, ss, csum, ssum, ess, htheta, m;
+    float invN, w, s, ss, csum, ssum, ess, htheta, m, rStart, rStep;
     float weightCdf[PF_N_TAG];
-    float randCdf[PF_N_TAG];
     
     s = 0.0f;
     ss = 0.0f;
@@ -48,7 +45,8 @@ void pfResample_resample(tag_t* tag, bcn_t* bcn, bcn_t* firstBcn, float range, f
     }
     ess = s * s / ss;
     
-    if (ess / PF_N_TAG < RESAMPLE_THRESH)
+    invN = 1.0f / PF_N_TAG;
+    if (ess * invN < RESAMPLE_THRESH)
     {
         csum /= s;
         ssum /= s;
@@ -57,13 +55,14 @@ void pfResample_resample(tag_t* tag, bcn_t* bcn, bcn_t* firstBcn, float range, f
         htheta = htheta < 1 - 1e-10 ? htheta : 1 - 1e-10;
         htheta = sqrtf(-logf(htheta) / ess);
         
-        _getRandCdf(randCdf, PF_N_TAG, s);
+        rStep = invN * s;
+        rStart = pfRandom_uniform() * rStep;
         
         i = 0;
         j = 0;
         while (i < PF_N_TAG)
         {
-            while (i < PF_N_TAG && randCdf[i] < weightCdf[j])
+            while (i < PF_N_TAG && (rStart + rStep * i) < weightCdf[j])
                 pfInit_spawnTagParticleFromOther(&tag->pTagBuf[i++], &tag->pTag[j], HXYZ, htheta);
             ++j;
         }
@@ -87,9 +86,8 @@ static void _resampleBcn(bcn_t* bcn, const tag_t* tag, float range, float stdRan
     int numSpawn, i, j, k;
     const tagParticle_t* tp;
     bcnParticle_t* bp;
-    float w, s, ss, ess, m;
+    float invN, w, s, ss, ess, m, rStart, rStep;
     float weightCdf[PF_N_BCN];
-    float randCdf[PF_N_BCN];
     
     for (k = 0; k < PF_N_TAG; ++k)
     {
@@ -105,19 +103,21 @@ static void _resampleBcn(bcn_t* bcn, const tag_t* tag, float range, float stdRan
         }
         ess = s * s / ss;
         
+        invN = 1.0f / PF_N_BCN;
         numSpawn = 0;
-        if (s / PF_N_BCN < WEIGHT_SPAWN_THRESH && range < RADIUS_SPAWN_THRESH)
+        if (s * invN < WEIGHT_SPAWN_THRESH && range < RADIUS_SPAWN_THRESH)
             numSpawn = (int)lroundf(PF_N_BCN * PCT_SPAWN);
         
-        if (ess / PF_N_BCN < RESAMPLE_THRESH || numSpawn > 0 || force)
+        if (ess * invN < RESAMPLE_THRESH || numSpawn > 0 || force)
         {
-            _getRandCdf(randCdf, PF_N_BCN, s);
+            rStep = invN * s;
+            rStart = pfRandom_uniform() * rStep;
             
             i = 0;
             j = 0;
             while (i < PF_N_BCN)
             {
-                while (i < PF_N_BCN && randCdf[i] < weightCdf[j])
+                while (i < PF_N_BCN && (rStart + rStep * i) < weightCdf[j])
                     pfInit_spawnBcnParticleFromOther(&bcn->pBcnBuf[i++], &bcn->pBcn[k][j], HXYZ);
                 ++j;
             }
@@ -135,25 +135,4 @@ static void _resampleBcn(bcn_t* bcn, const tag_t* tag, float range, float stdRan
                 bcn->pBcn[k][i].w *= m;
         }
     }
-}
-
-static void _getRandCdf(float* buf, int count, float s)
-{
-    int i;
-    
-    for (i = 0; i < count; ++i)
-        buf[i] = pfRandom_uniform() * s;
-    qsort(buf, count, sizeof(buf[0]), _floatCmp);
-}
-
-static int _floatCmp(const void* a, const void* b)
-{
-    float* x = (float*)a;
-    float* y = (float*)b;
-    
-    if (*x < *y)
-        return -1;
-    if (*x > *y)
-        return 1;
-    return 0;
 }
