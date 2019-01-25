2c68ca0000af9bb7b6182b73098a432772a60d38
John <jmiller4@andrew.cmu.edu>
2019-01-25 18:08:23 -0500

    [update] implement jni depositVio and depositUwb

--- ArSlam/app/src/main/jni/slam3d-jni.c
+++ ArSlam/app/src/main/jni/slam3d-jni.c
@@ -39,8 +39,11 @@ JNIEXPORT void JNICALL Java_com_example_arslam_Slam3dJni_particleFilter_depositV
 }
 
 JNIEXPORT void JNICALL Java_com_example_arslam_Slam3dJni_particleFilter_depositUwb(
-        JNIEnv* env, jobject thiz, jlong* pf, jlong* bcn, jfloat range, jfloat stdRange) {
-    particleFilter_depositUwb(pf, bcn, range, stdRange);
+        JNIEnv* env, jobject thiz, jlong* pf, jlong* bcn, jfloat range, jfloat stdRange, jlongArray bcnArray) {
+    bcn_t** allBcns = (bcn_t**)(*env)->GetLongArrayElements(env, bcnArray, NULL);
+    int numBcns = (int)(*env)->GetArrayLength(env, bcnArray);
+    particleFilter_depositUwb((particleFilter_t*)pf, (bcn_t*)bcn, (float)range, (float)stdRange, allBcns, numBcns);
+    (*env)->ReleaseLongArrayElements(env, bcnArray, (jlong*)allBcns, 0);
 }
 
 JNIEXPORT void JNICALL Java_com_example_arslam_Slam3dJni_particleFilter_getTagLoc(
