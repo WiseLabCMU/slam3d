8f5ce039cc1abcf44b9a3fe4fa29ba5487ebd1be
CalculatorSP <mike.handidate@gmail.com>
2019-01-29 22:11:35 -0500

    [cleanup]

--- ArSlam/app/src/main/java/com/example/arslam/MainActivity.java
+++ ArSlam/app/src/main/java/com/example/arslam/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.arslam;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
 import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
@@ -22,7 +18,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
@@ -152,20 +147,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_selectDevice) {
+        if (item.getItemId() == R.id.action_selectDevice) {
             AlertDialog alertDialog = new AlertDialog.Builder(this)
                     .setTitle("Paired Devices:")
                     .setCancelable(false)
