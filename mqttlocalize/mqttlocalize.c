644d1e53429f0ea71ac48800e59f01a4ac5d263a
Nuno <3504501+nampereira@users.noreply.github.com>
2019-09-03 08:37:29 -0400

    Pubsub from mqtt

--- mqttlocalize/mqttlocalize.c
+++ mqttlocalize/mqttlocalize.c
@@ -10,8 +10,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 #include "particleFilter.h"
+
+#include "MQTTClient.h"
 
 #define DATA_DIR            "../sampledata/"
 #define TRACE_DIR           DATA_DIR "cic/0/"
@@ -26,104 +31,121 @@
 #define TAG_OUT_FILE        TRACE_DIR "tag.csv"
 #define LINE_LEN            (1024)
 
-static uint8_t _getVio(FILE* vioFile, double* t, float* x, float* y, float* z, uint8_t skipToWaypoint);
-static uint8_t _getUwb(FILE* uwbFile, double* t, uint8_t* b, float* r, uint8_t skipToWaypoint);
+#define ADDRESS     "oz.andrew.cmu.edu:1883"
+#define CLIENTID    "localize"
+#define PUB_TOPIC    "/topic/render/camera_5432/rig"
+#define QOS         1
+#define TIMEOUT     10000L
+
+
+#define TOPIC       "MQTT Examples"
+
+#define FRMT_TAG_LOC_MSG "%f,%f,%f,%f,%f,%f,%f"
+
+//static uint8_t _getVio(FILE* vioFile, double* t, float* x, float* y, float* z, uint8_t skipToWaypoint);
+//static uint8_t _getUwb(FILE* uwbFile, double* t, uint8_t* b, float* r, uint8_t skipToWaypoint);
 static void _getDeployment(FILE* deployFile, float deployment[NUM_BCNS][3]);
-static void _writeTagLoc(FILE* outFile, double t, float x, float y, float z, float theta);
+//static void _writeTagLoc(FILE* outFile, double t, float x, float y, float z, float theta);
+static void _publishTagLoc(MQTTClient client, char *topic, double t, float x, float y, float z, float theta);
+
+void delivered(void *context, MQTTClient_deliveryToken dt);
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message);
+void connlost(void *context, char *cause);
 
 static particleFilterLoc_t _particleFilter;
+static float deployment[NUM_BCNS][3];
 
-int main(int argc, char** argv)
+static char *topicName_VIO;
+static char *topicName_UWB;
+static char *topicName_LocOut;
+
+static MQTTClient client;
+
+int main(int argc, char* argv[])
 {
-    FILE* vioFile;
-    FILE* uwbFile;
     FILE* deployFile;
-    FILE* tagOutFile;
-    float deployment[NUM_BCNS][3];
-    double vioT, uwbT, outT;
-    float vioX, vioY, vioZ, uwbR, outX, outY, outZ, outTheta;
-    uint8_t uwbB, haveVio, haveUwb;
+    double outT;
+    float outX, outY, outZ, outTheta;
+
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    int rc;
+    int ch;
+
+    if (argc <4) printf("Usage: %s <Subscribe_VIO_Topic> <Subscribe_UWB_Topic> <Publish_Loc_Topic>\n", argv[0]);
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID,
+        MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+
+    MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
+
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(EXIT_FAILURE);
+    }
+
+    topicName_VIO = argv[1];
+    topicName_UWB = argv[2];
+    topicName_LocOut = argv[3];
+
+    printf("Subscribing to topic %s\nfor client %s using QoS%d\n\n", topicName_VIO, CLIENTID, QOS);
+    MQTTClient_subscribe(client, argv[1], QOS);
+    printf("Subscribing to topic %s\nfor client %s using QoS%d\n\n", topicName_UWB, CLIENTID, QOS);    
+    MQTTClient_subscribe(client, topicName_UWB, QOS);
 
     printf("Starting localization\n");
-    vioFile = fopen(VIO_FILE, "r");
-    uwbFile = fopen(UWB_FILE, "r");
-    tagOutFile = fopen(TAG_OUT_FILE, "w");
     particleFilterLoc_init(&_particleFilter);
-
     deployFile = fopen(DEPLOY_FILE, "r");
     _getDeployment(deployFile, deployment);
     fclose(deployFile);
-
     printf("Initialized\n");
 
-    haveVio = _getVio(vioFile, &vioT, &vioX, &vioY, &vioZ, SKIP_TO_WAYPOINT);
-    haveUwb = _getUwb(uwbFile, &uwbT, &uwbB, &uwbR, SKIP_TO_WAYPOINT);
-    while (haveVio || haveUwb)
+    printf("s=%d", sizeof(particleFilterLoc_t)/1000);
+/*
+    do 
     {
-        if (haveVio && (!haveUwb || vioT < uwbT))
-        {
-            particleFilterLoc_depositVio(&_particleFilter, vioT, vioX, vioY, vioZ, 0.0f);
-            if (particleFilterLoc_getTagLoc(&_particleFilter, &outT, &outX, &outY, &outZ, &outTheta))
-                _writeTagLoc(tagOutFile, outT, outX, outY, outZ, outTheta);
-            haveVio = _getVio(vioFile, &vioT, &vioX, &vioY, &vioZ, 0);
-        }
-        else if (haveUwb)
-        {
-            uwbR -= UWB_BIAS;
-            if (uwbR > 0.0f && uwbR < 30.0f)
-                particleFilterLoc_depositRange(&_particleFilter, deployment[uwbB][0], deployment[uwbB][1], deployment[uwbB][2], uwbR, UWB_STD);
-            haveUwb = _getUwb(uwbFile, &uwbT, &uwbB, &uwbR, 0);
-        }
-    }
-    printf("Finished localization\n");
+        ch = getchar();
 
-    fclose(vioFile);
-    fclose(uwbFile);
-    fclose(tagOutFile);
-
-    printf("Done\n");
-    return 0;
-}
-
-static uint8_t _getVio(FILE* vioFile, double* t, float* x, float* y, float* z, uint8_t skipToWaypoint)
-{
-    static char _lineBuf[LINE_LEN];
-    char waypoint;
-
+    } while(ch!='Q' && ch != 'q');
+ */
     do
     {
-        if (fgets(_lineBuf, LINE_LEN, vioFile) == NULL)
-            return 0;
-        *t = atof(strtok(_lineBuf, ","));
-        strtok(NULL, ","); // Skip "position" or "orientation" string
-        waypoint = strtok(NULL, ",")[0]; // Skip waypoint number
-        strtok(NULL, ","); // Skip accuracy number
-        *y = (float)atof(strtok(NULL, ","));    // VIO on iOS is reported in a different order (y, z, x)
-        *z = (float)atof(strtok(NULL, ","));
-        *x = (float)atof(strtok(NULL, ",\n"));
-        fgets(_lineBuf, LINE_LEN, vioFile); // Skip line for orientation
-    } while (skipToWaypoint && waypoint < '4');
+        if (particleFilterLoc_getTagLoc(&_particleFilter, &outT, &outX, &outY, &outZ, &outTheta))
+            _publishTagLoc(client, topicName_LocOut, outT, outX, outY, outZ, outTheta);
+        sleep(1);
+    } while(1);
+
+    MQTTClient_unsubscribe(client, TOPIC);
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+    return rc;
+}
+
+static uint8_t _getVio(char *_lineBuf, double* t, float* x, float* y, float* z)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL); 
+    
+    strtok(_lineBuf, ",");
+    *t = tv.tv_sec + tv.tv_usec / 1000000.0;
+    *y = (float)atof(strtok(NULL, ","));    
+    *z = (float)atof(strtok(NULL, ","));
+    *x = (float)atof(strtok(NULL, ","));
 
     return 1;
 }
 
-static uint8_t _getUwb(FILE* uwbFile, double* t, uint8_t* b, float* r, uint8_t skipToWaypoint)
+static uint8_t _getUwb(char *_lineBuf, double* t, uint8_t* b, float* r)
 {
-    static char _lineBuf[LINE_LEN];
-    char waypoint;
+    struct timeval tv;
+    gettimeofday(&tv, NULL); 
+    
+    *t = tv.tv_sec + tv.tv_usec / 1000000.0;
+    *b = atoi(strtok(_lineBuf, ",")); 
+    *r = (float)atof(strtok(NULL, ","));
 
-    do
-    {
-        if (fgets(_lineBuf, LINE_LEN, uwbFile) == NULL)
-            return 0;
-        *t = atof(strtok(_lineBuf, ","));
-        strtok(NULL, ","); // Skip "uwb_range" string
-        waypoint = strtok(NULL, ",")[0]; // Skip waypoint number
-        *b = strtok(NULL, ",")[0] - 'a';
-        *r = (float)atof(strtok(NULL, ",\n"));
-    } while (skipToWaypoint && waypoint < '4');
-
-    assert(*b < NUM_BCNS);
     return 1;
 }
 
@@ -154,4 +176,64 @@ static void _writeTagLoc(FILE* outFile, double t, float x, float y, float z, flo
         printedHeaders = 1;
     }
     fprintf(outFile, "%lf,%f,%f,%f,%f\n", t, x, y, z, theta);
+}
+
+static void _publishTagLoc(MQTTClient client, char *topic, double t, float x, float y, float z, float theta)
+{
+    MQTTClient_deliveryToken token;
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    int rc;
+    char str_msg[100];
+
+    snprintf(str_msg, 100, FRMT_TAG_LOC_MSG, y, z, x, 0.0f, 0.0f, sinf(theta/2), cosf(theta/2));
+
+    pubmsg.payload = str_msg;
+    pubmsg.payloadlen = strlen(str_msg);
+    pubmsg.qos = QOS;
+    pubmsg.retained = 0;
+    MQTTClient_publishMessage(client, topic, &pubmsg, &token);
+    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+    //printf("Message with delivery token %d delivered\n", token);
+}
+
+void delivered(void *context, MQTTClient_deliveryToken dt)
+{
+    printf("Message with token value %d delivery confirmed\n", dt);
+    //deliveredtoken = dt;
+}
+
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
+{
+    double vioT, uwbT, outT;
+    float vioX, vioY, vioZ, uwbR, outX, outY, outZ, outTheta;
+    uint8_t uwbB;
+    char payload_str[LINE_LEN];
+
+    assert(message->payloadlen < LINE_LEN-1);
+    memcpy(payload_str, message->payload, message->payloadlen);
+    payload_str[message->payloadlen] = '\0';
+
+    printf("#topic: %s; message:%s", topicName, payload_str);  
+
+    if (strncmp(topicName, topicName_VIO, strlen(topicName_VIO)) == 0) {
+        _getVio(payload_str, &vioT, &vioX, &vioY, &vioZ);
+        printf(" => VIO:%lf,%f,%f,%f\n", vioT, vioX, vioY, vioZ);
+        particleFilterLoc_depositVio(&_particleFilter, vioT, vioX, vioY, vioZ, 0.0f);
+    } else if (strncmp(topicName, topicName_UWB, strlen(topicName_UWB)) == 0) {
+        _getUwb(payload_str, &uwbT, &uwbB, &uwbR);
+        printf(" => UWB:%lf,%d,%f\n", uwbT, uwbB, uwbR);
+        assert(uwbB<NUM_BCNS);
+        uwbR -= UWB_BIAS;
+        if (uwbR > 0.0f && uwbR < 30.0f)
+            particleFilterLoc_depositRange(&_particleFilter, deployment[uwbB][0], deployment[uwbB][1], deployment[uwbB][2], uwbR, UWB_STD);
+    }
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    return 1;
+}
+
+void connlost(void *context, char *cause)
+{
+    printf("\nConnection lost\n");
+    printf("     cause: %s\n", cause);
 }
