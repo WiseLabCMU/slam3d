73bb2033f2f06e2e92f53dc59cf4a9f7f75d2218
Nuno Pereira <nampereira@gmail.com>
2020-01-24 11:26:32 -0500

    Added script to start solver

---
+++ mqttlocalize/fixed_camera_ipad_pot_solver.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+IPADNAME="pixel"
+SCENENAME="earth"
+
+eval CAMERAID="camera_${IPADNAME}_${IPADNAME}"
+eval IPADVIO="/topic/vio/${CAMERAID}"
+eval IPADUWB="topic/uwb_range_mud"
+eval IPADLOCATION="/topic/loc/${CAMERAID}"
+eval IPADVIZ="realm/s/$SCENENAME/"
+
+
+# check if we requesting ranges from tags
+if ! pidof -x "uwb_ctrl_publish.sh" >/dev/null; then
+    echo "Start requesting ranges from tags"
+    ./uwb_ctrl_publish.sh >/dev/null & # script that periodically tells the tags to publish ranges
+fi
+
+./mqttlocalize ${IPADVIO} ${IPADUWB} ${IPADLOCATION} ${IPADVIZ} ${CAMERAID}
+# ./mqttlocalize /topic/vio/camera_ipad_ipad topic/uwb_range_pot /topic/loc/camera_ipad_ipad /topic/render/camera_ipad_ipad/rig
