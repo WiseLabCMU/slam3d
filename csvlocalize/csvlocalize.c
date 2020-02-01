b9709796fe7a4bad17c6ea016fce7bb37630598a
John <jmiller4@andrew.cmu.edu>
2020-02-01 14:26:04 -0500

    [update] csvlocalize now works with mqttlogger log format

--- csvlocalize/csvlocalize.c
+++ csvlocalize/csvlocalize.c
@@ -13,16 +13,29 @@
 
 #include "particleFilter.h"
 
-#define DATA_DIR            "../sampledata/"
-#define TRACE_DIR           DATA_DIR "cic/0/"
-#define NUM_BCNS            (12)
+//#define DATA_DIR            "../sampledata/"
+//#define TRACE_DIR           DATA_DIR "cic/0/"
+//#define NUM_BCNS            (12)
+//#define UWB_STD             (0.1f)
+//#define UWB_BIAS            (0.4f)
+//#define SKIP_TO_WAYPOINT    (1)
+//
+//#define VIO_FILE            TRACE_DIR "vio.csv"
+//#define UWB_FILE            TRACE_DIR "uwb.csv"
+//#define DEPLOY_FILE         TRACE_DIR "deploy.csv"
+//#define TAG_OUT_FILE        TRACE_DIR "tag.csv"
+//#define LINE_LEN            (1024)
+
+#define DATA_DIR            "../mqttlogger/"
+#define TRACE_DIR           DATA_DIR
+#define NUM_BCNS            (4)
 #define UWB_STD             (0.1f)
-#define UWB_BIAS            (0.4f)
-#define SKIP_TO_WAYPOINT    (1)
+#define UWB_BIAS            (0.2f)
+#define SKIP_TO_WAYPOINT    (0)
 
 #define VIO_FILE            TRACE_DIR "vio.csv"
 #define UWB_FILE            TRACE_DIR "uwb.csv"
-#define DEPLOY_FILE         TRACE_DIR "deploy.csv"
+#define DEPLOY_FILE         TRACE_DIR "../sampledata/arena/deploy.csv"
 #define TAG_OUT_FILE        TRACE_DIR "tag.csv"
 #define LINE_LEN            (1024)
 
@@ -85,43 +98,72 @@ int main(int argc, char** argv)
     return 0;
 }
 
+//static uint8_t _getVio(FILE* vioFile, double* t, float* x, float* y, float* z, uint8_t skipToWaypoint)
+//{
+//    static char _lineBuf[LINE_LEN];
+//    char waypoint;
+//
+//    do
+//    {
+//        if (fgets(_lineBuf, LINE_LEN, vioFile) == NULL)
+//            return 0;
+//        *t = atof(strtok(_lineBuf, ","));
+//        strtok(NULL, ","); // Skip "position" or "orientation" string
+//        waypoint = strtok(NULL, ",")[0]; // Skip waypoint number
+//        strtok(NULL, ","); // Skip accuracy number
+//        *y = (float)atof(strtok(NULL, ","));    // VIO on iOS is reported in a different order (y, z, x)
+//        *z = (float)atof(strtok(NULL, ","));
+//        *x = (float)atof(strtok(NULL, ",\n"));
+//        fgets(_lineBuf, LINE_LEN, vioFile); // Skip line for orientation
+//    } while (skipToWaypoint && waypoint < '4');
+//
+//    return 1;
+//}
+
 static uint8_t _getVio(FILE* vioFile, double* t, float* x, float* y, float* z, uint8_t skipToWaypoint)
 {
     static char _lineBuf[LINE_LEN];
-    char waypoint;
 
-    do
-    {
-        if (fgets(_lineBuf, LINE_LEN, vioFile) == NULL)
-            return 0;
-        *t = atof(strtok(_lineBuf, ","));
-        strtok(NULL, ","); // Skip "position" or "orientation" string
-        waypoint = strtok(NULL, ",")[0]; // Skip waypoint number
-        strtok(NULL, ","); // Skip accuracy number
-        *y = (float)atof(strtok(NULL, ","));    // VIO on iOS is reported in a different order (y, z, x)
-        *z = (float)atof(strtok(NULL, ","));
-        *x = (float)atof(strtok(NULL, ",\n"));
-        fgets(_lineBuf, LINE_LEN, vioFile); // Skip line for orientation
-    } while (skipToWaypoint && waypoint < '4');
+    if (fgets(_lineBuf, LINE_LEN, vioFile) == NULL)
+        return 0;
+
+    *t = atof(strtok(_lineBuf, ","));
+    *y = (float)atof(strtok(NULL, ","));    // VIO on iOS is reported in a different order (y, z, x)
+    *z = (float)atof(strtok(NULL, ","));
+    *x = (float)atof(strtok(NULL, ",\n"));
 
     return 1;
 }
 
+//static uint8_t _getUwb(FILE* uwbFile, double* t, uint8_t* b, float* r, uint8_t skipToWaypoint)
+//{
+//    static char _lineBuf[LINE_LEN];
+//    char waypoint;
+//
+//    do
+//    {
+//        if (fgets(_lineBuf, LINE_LEN, uwbFile) == NULL)
+//            return 0;
+//        *t = atof(strtok(_lineBuf, ","));
+//        strtok(NULL, ","); // Skip "uwb_range" string
+//        waypoint = strtok(NULL, ",")[0]; // Skip waypoint number
+//        *b = strtok(NULL, ",")[0] - 'a';
+//        *r = (float)atof(strtok(NULL, ",\n"));
+//    } while (skipToWaypoint && waypoint < '4');
+//
+//    assert(*b < NUM_BCNS);
+//    return 1;
+//}
+
 static uint8_t _getUwb(FILE* uwbFile, double* t, uint8_t* b, float* r, uint8_t skipToWaypoint)
 {
     static char _lineBuf[LINE_LEN];
-    char waypoint;
 
-    do
-    {
-        if (fgets(_lineBuf, LINE_LEN, uwbFile) == NULL)
-            return 0;
-        *t = atof(strtok(_lineBuf, ","));
-        strtok(NULL, ","); // Skip "uwb_range" string
-        waypoint = strtok(NULL, ",")[0]; // Skip waypoint number
-        *b = strtok(NULL, ",")[0] - 'a';
-        *r = (float)atof(strtok(NULL, ",\n"));
-    } while (skipToWaypoint && waypoint < '4');
+    if (fgets(_lineBuf, LINE_LEN, uwbFile) == NULL)
+        return 0;
+    *t = atof(strtok(_lineBuf, ","));
+    *b = (uint8_t)atoi(strtok(NULL, ","));
+    *r = (float)atof(strtok(NULL, ",\n"));
 
     assert(*b < NUM_BCNS);
     return 1;
@@ -139,11 +181,22 @@ static void _getDeployment(FILE* deployFile, float deployment[NUM_BCNS][3])
             return;
         b = (uint8_t)atoi(strtok(_lineBuf, ","));
         assert(b < NUM_BCNS);
-        deployment[b][0] = (float)atof(strtok(NULL, ","));
         deployment[b][1] = (float)atof(strtok(NULL, ","));
-        deployment[b][2] = (float)atof(strtok(NULL, ",\n"));
+        deployment[b][2] = (float)atof(strtok(NULL, ","));
+        deployment[b][0] = (float)atof(strtok(NULL, ",\n"));
     }
 }
+
+//static void _writeTagLoc(FILE* outFile, double t, float x, float y, float z, float theta)
+//{
+//    static uint8_t printedHeaders = 0;
+//    if (!printedHeaders)
+//    {
+//        fprintf(outFile, "t,x,y,z,theta\n");
+//        printedHeaders = 1;
+//    }
+//    fprintf(outFile, "%lf,%f,%f,%f,%f\n", t, x, y, z, theta);
+//}
 
 static void _writeTagLoc(FILE* outFile, double t, float x, float y, float z, float theta)
 {
@@ -153,5 +206,5 @@ static void _writeTagLoc(FILE* outFile, double t, float x, float y, float z, flo
         fprintf(outFile, "t,x,y,z,theta\n");
         printedHeaders = 1;
     }
-    fprintf(outFile, "%lf,%f,%f,%f,%f\n", t, x, y, z, theta);
+    fprintf(outFile, "%lf,%f,%f,%f,%f\n", t, y, z, x, theta);
 }
