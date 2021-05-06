3e34e95b122a6ea1de71e13d2cb653b958924725
CalculatorSP <mike.handidate@gmail.com>
2021-05-06 19:31:56 -0400

    [update] add main function

--- gt-sync/gt-sync.py
+++ gt-sync/gt-sync.py
@@ -2,33 +2,179 @@
 ''' Synchronize users for ground truth collection with apriltags
 '''
 
-from arena import *
-import random
+import arena
+from datetime import datetime
+import getopt
+import json
+import numpy as np
+import os
+import pose
+import sys
 
-MIN_DISPLACEMENT = 0.5
-LINE_TTL = 5
+BROKER = 'oz.andrew.cmu.edu'
+REALM = 'realm'
+TOPIC_DETECT = REALM + '/g/a/#'
+TOPIC_VIO = '/topic/vio/#'
+TOPIC_UWB = REALM + '/g/uwb/#'
+TIME_FMT = '%Y-%m-%dT%H:%M:%S.%fZ'
+TIME_FMT_UWB = '%Y-%m-%dT%H:%M:%S.%f'
+OUTFILE = os.path.join('localization', 'gt', datetime.now().strftime('%Y-%m-%d_%H_%M_%S') + '.json')
+
+STATE_WALK = 0
+STATE_FINDTAG = 1
+STATE_WAIT = 2
+COLOR_WALK = (0, 255, 0)
+COLOR_FINDTAG = (255, 0, 0)
+COLOR_WAIT = (0, 0, 255)
+MOVE_THRESH = .05   # 5cm
+ROT_THRESH = .087   # 5deg
+TIME_THRESH = 3     # 3sec
+DTAG_ERROR_THRESH = 5e-6    # tag detection error units?
+TIME_INTERVAL = 5           # 5sec
+
+users = {}
+arenanames = {}
+last_detection = datetime.min
 
 
-class CameraState(Object):
-    def __init__(self, camera):
-        self.camera = camera
-        self.prev_pos = camera.data.position
-        self.line_color = Color(
-                random.randint(0,255),
-                random.randint(0,255),
-                random.randint(0,255)
-            )
+class SyncUser:
+    def __init__(self, config):
+        self.hud = arena.Circle(parent=config.client_id,
+                                position=(0, 0, -0.5),
+                                rotation=(0, 0, 0, 1),
+                                scale=(0.02, 0.02, 0.02)
+                                )
+        self.arenaname = config.arenaname
+        self.reset()
 
-    @property
-    def curr_pos(self):
-        return self.camera.data.position
+    def reset(self):
+        self.pose = None
+        self.last_time = datetime.min
+        self.state = STATE_WALK
+        self.hud.update(color=COLOR_WALK)
 
-    @property
-    def displacement(self):
-        return self.prev_pos.distance_to(self.curr_pos)
+    def on_tag_detect(self, cam_pose, vio, time, debug):
+        self.pose = cam_pose
+        self.last_vio = vio
+        self.last_time = time
+        self.debug = debug
+        if self.state == STATE_FINDTAG:
+            self.state = STATE_WAIT
+            self.hud.update(color=COLOR_WAIT)
+
+    def on_vio(self, vio, time):
+        if self.state == STATE_WAIT:
+            pos_diff, rot_diff = pose.pose_diff(vio, self.last_vio)
+            time_diff = (time - self.last_time).total_seconds()
+            if pos_diff > MOVE_THRESH or rot_diff > ROT_THRESH or time_diff > TIME_THRESH:
+                self.state = STATE_FINDTAG
+                self.hud.update(color=COLOR_FINDTAG)
+
+    def on_timer(self):
+        if self.state == STATE_WALK:
+            self.state = STATE_FINDTAG
+            self.hud.update(color=COLOR_FINDTAG)
 
 
-cam_states = []
+class StaticUser(SyncUser):
+    def __init__(self, config):
+        self.arenaname = config.arenaname
+        self.pose = np.array(config.pose)
+        self.state = STATE_WAIT
+        self.debug = 'static'
+
+    def reset(self):
+        pass
+
+    def on_tag_detect(self, cam_pose, vio, time):
+        pass
+
+    def on_vio(self, vio, time):
+        pass
+
+    def on_timer(self):
+        pass
+
+
+def printhelp():
+    print('gt-sync.py -s <scene> -u <userfile>')
+    print('   ex: python3 gt-sync.py -s myScene -u users.json')
+
+
+def on_tag_detect(msg):
+    global users
+    global last_detection
+    json_msg = json.loads(msg.payload.decode('utf-8'), object_hook=dict_to_sns)
+    client_id = msg.topic.split('/')[-1]
+    if client_id not in users:
+        return
+    if hasattr(json_msg, 'detections'):
+        dtag = json_msg.detections[0]
+        if not hasattr(dtag, 'refTag'):
+            print('tag not in atlas:', dtag.id)
+            return
+        cam_pose, dtag_error = pose.get_cam_pose(json_msg)
+        if dtag_error > DTAG_ERROR_THRESH:
+            return
+        vio_pose = pose.get_vio_pose(json_msg)
+        time = datetime.strptime(json_msg.timestamp, TIME_FMT)
+        users[client_id].on_tag_detect(cam_pose, vio_pose, time, msg.payload.decode('utf-8'))
+        if all(users[u].state == STATE_WAIT for u in users):
+            poselist = [
+                {
+                    'user': users[u].arenaname,
+                    'pose': users[u].pose.tolist(),
+                    'dbg':  users[u].debug
+                }
+                for u in users
+            ]
+            data = {
+                'timestamp': time.strftime(TIME_FMT_UWB),
+                'type':      'gt',
+                'poses':     poselist
+            }
+            print(data)
+            with open(OUTFILE, 'a') as outfile:
+                outfile.write(json.dumps(data))
+                outfile.write(',\n')
+            last_detection = time
+            for u in users:
+                users[u].reset()
+
+
+def on_vio(msg):
+    global users
+    json_msg = json.loads(msg.payload.decode('utf-8'), object_hook=dict_to_sns)
+    client_id = msg.topic.split('/')[-1]
+    if client_id not in users:
+        return
+    if hasattr(json_msg, 'object_id') and json_msg.object_id.endswith('_local'):
+        vio_pose = pose.get_vio_pose(json_msg)
+        time = datetime.strptime(json_msg.timestamp, TIME_FMT)
+        users[client_id].on_vio(vio_pose, time)
+        data = {'timestamp': time.strftime(TIME_FMT_UWB), 'type': 'vio', 'user': users[client_id].arenaname, 'pose': vio_pose.tolist()}
+        with open(OUTFILE, 'a') as outfile:
+            outfile.write(json.dumps(data))
+            outfile.write(',\n')
+
+
+def on_uwb(msg):
+    json_msg = json.loads(msg.payload.decode('utf-8'), object_hook=dict_to_sns)
+    time = datetime.strptime(json_msg.timestamp, TIME_FMT_UWB)
+    if not json_msg.src in arenanames:
+        print('User not tracked:', json_msg.src)
+        return
+    if not json_msg.dst in arenanames:
+        print('User not tracked:', json_msg.dst)
+        return
+    src = arenanames[json_msg.src]
+    dst = arenanames[json_msg.dst]
+    rng = float(json_msg.distance)
+    rssi = int(json_msg.ble_rssi)
+    data = {'timestamp': time.strftime(TIME_FMT_UWB), 'type': 'uwb', 'src': src, 'dst': dst, 'range': rng, 'ble_rssi': rssi}
+    with open(OUTFILE, 'a') as outfile:
+        outfile.write(json.dumps(data))
+        outfile.write(',\n')
 
 
 def user_join_callback(scene, cam, msg):
@@ -38,23 +184,57 @@ def user_join_callback(scene, cam, msg):
     cam_states += [cam_state]
 
 
-scene = Scene(host="arenaxr.org", realm="realm", scene="example")
+scene = arena.Scene(host="arenaxr.org", realm="realm", scene="example")
 scene.user_join_callback = user_join_callback
 
 
-@scene.run_forever(interval_ms=200)
-def line_follow():
-    for cam_state in cam_states:
-        if cam_state.displacement >= MIN_DISPLACEMENT:
-            line = ThickLine(
-                    color=cam_state.line_color,
-                    path=(cam_state.prev_pos, cam_state.curr_pos),
-                    lineWidth=300,
-                    ttl=LINE_TTL
-                )
-            scene.add_object(line)
-            # the camera's position gets automatically updated by arena-py!
-            cam_state.prev_pos = cam_state.curr_pos
+@scene.run_forever(interval_ms=TIME_INTERVAL*1000)
+def prompt_users():
+    global users
+    for u in users:
+        users[u].on_timer()
+
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hs:u:', ['scene=', 'userfile='])
+    except getopt.GetoptError:
+        printhelp()
+        sys.exit(1)
+
+    scene = None
+    userfile = None
+    for opt, arg in opts:
+        if opt == '-h':
+            printhelp()
+            sys.exit(1)
+        elif opt in ('-s', '--scene'):
+            scene = arg
+        elif opt in ('-u', '--userfile'):
+            userfile = arg
+        else:
+            printhelp()
+            sys.exit(1)
+    if scene is None or userfile is None:
+        printhelp()
+        sys.exit(1)
+
+    with open(userfile, 'r') as f:
+        config = json.load(f, object_hook=dict_to_sns)
+
+    arena.init(BROKER, REALM, scene)
+    for user in config:
+        arenanames[user.uwbname] = user.arenaname
+        if user.static:
+            users[user.client_id] = StaticUser(user)
+        else:
+            users[user.client_id] = SyncUser(user)
+            print("Go to URL: https://xr.andrew.cmu.edu/?networkedTagSolver=true&scene=" + scene + "&fixedCamera=" + user.arenaname)
+
+    scene.add_topic(TOPIC_DETECT, on_tag_detect)
+    scene.add_topic(TOPIC_VIO, on_vio)
+    scene.add_topic(TOPIC_UWB, on_uwb)
+    arena.handle_events()
 
 
 scene.run_tasks() # will block
---
+++ gt-sync/pose.py
@@ -0,0 +1,140 @@
+import math
+import numpy as np
+from scipy.spatial.transform import Rotation
+
+
+def pose_to_matrix4(pos, rotq):
+    mat = np.identity(4)
+    if hasattr(rotq, '_x'):
+        mat[0:3, 0:3] = Rotation.from_quat([
+            rotq._x, rotq._y, rotq._z, rotq._w]).as_matrix()
+    else:
+        mat[0:3, 0:3] = Rotation.from_quat([
+            rotq.x, rotq.y, rotq.z, rotq.w]).as_matrix()
+    mat[0:3, 3] = [pos.x, pos.y, pos.z]
+    return mat
+
+
+def matrix4_to_pose(mat):
+    pos = mat[0:3, 3]
+    rotq = Rotation.from_matrix(mat[0:3, 0:3]).as_quat()
+    return pos, rotq
+
+
+'''
+Get pose of object with error artificially added
+    cam_pose:   4x4 matrix, pose of camera in global frame
+    obj_pose:   4x4 matrix, true pose of obj in global frame
+    error:      positive float, amount of error to add as a fraction of distance between cam and obj
+
+    return:     4x4 matrix, pose of obj in global frame with error added
+'''
+def pose_with_error(cam_pose, obj_pose, error):
+    return obj_pose
+
+
+def dtag_pose_to_matrix4(dtag_pose):
+    FLIP = np.diag([1, -1, -1, 1])
+    mat = np.identity(4)
+    # Correct for column-major format of detected tag pose
+    mat[0:3, 0:3] = np.array(dtag_pose.R).T
+    mat[0:3, 3] = dtag_pose.t
+    # Swap x/y axis of detected tag coordinate system
+    mat = FLIP @ mat @ FLIP
+    return mat
+
+
+def reftag_pose_to_matrix4(reftag_pose):
+    # comes in as col-major
+    arr = reftag_pose.elements
+    mat = np.array([arr[0:4], arr[4:8], arr[8:12], arr[12:16]]).T
+    return mat
+
+
+def pose_diff(pose2, pose1):
+    posediff = pose2 @ np.linalg.inv(pose1)
+    pos_diff = np.linalg.norm(posediff[0:3, 3])
+    rot_diff = math.acos((np.trace(posediff[0:3, 0:3]) - 1) / 2)
+    return pos_diff, rot_diff
+
+
+def resolve_pose_ambiguity(pose1, err1, pose2, err2, vio, tagpose):
+    vertical_vector = tagpose[0:3, 0:3].T @ np.array([[0, 1, 0]]).T
+    pose1_vertical = pose1[0:3, 0:3] @ vertical_vector
+    pose2_vertical = pose2[0:3, 0:3] @ vertical_vector
+    vio_vertical = vio[0:3, 0:3].T @ np.array([[0, 1, 0]]).T
+    pose1_vertical = pose1_vertical / np.linalg.norm(pose1_vertical)
+    pose2_vertical = pose2_vertical / np.linalg.norm(pose2_vertical)
+    vio_vertical = vio_vertical / np.linalg.norm(vio_vertical)
+    valign1 = np.dot(pose1_vertical.T, vio_vertical)
+    valign2 = np.dot(pose2_vertical.T, vio_vertical)
+    if valign1 >= valign2 and err1 <= err2:
+        return pose1, err1
+    if valign2 >= valign1 and err2 <= err1:
+        return pose2, err2
+    if valign1 >= valign2:
+        return pose1, 99999999.9
+    return pose2, 99999999.9
+
+
+def _test_resolve_pose_ambiguity():
+    test_pose1 = np.array(
+        [[.72, -.01, -.69, -.06],
+         [-.05, 1.00, -.06, -.03],
+         [.69, .07, .72, -1.15],
+         [.00, .00, .00, 1.00]])
+    test_error1 = 1e-6
+    test_pose2 = np.array(
+        [[.65, -.02, .76, -.05],
+         [-.09, .99, .10, -.03],
+         [-.75, -.13, .65, -1.14],
+         [.00, .00, .00, 1.00]])
+    test_error2 = 181e-6
+    test_vio = np.identity(4)
+    test_tagpose = np.array(
+        [[-0.9689, 0.0076, 0.2473, 1.382],
+         [0.0079, 1, 0, 5.2782],
+         [-0.2473, 0.0019, -0.9689, 13.7963],
+         [0, 0, 0, 1]])
+    test_pose, test_error = resolve_pose_ambiguity(
+        test_pose1, test_error1, test_pose2, test_error2, test_vio, test_tagpose)
+    print(test_pose)
+    print(test_error)
+
+
+def get_dtag_pose(msg):
+    detected_tag = msg.detections[0]
+    vio_pose = pose_to_matrix4(msg.vio.position, msg.vio.rotation)
+    dtag_pose1 = dtag_pose_to_matrix4(detected_tag.pose)
+    dtag_pose2 = dtag_pose_to_matrix4(detected_tag.pose.asol)
+    dtag_error1 = detected_tag.pose.e
+    dtag_error2 = detected_tag.pose.asol.e
+    reftag_pose = reftag_pose_to_matrix4(detected_tag.refTag.pose)
+    if detected_tag.pose.asol.uniquesol:
+        return resolve_pose_ambiguity(dtag_pose1, dtag_error1, dtag_pose2, dtag_error2, vio_pose, reftag_pose)
+    return dtag_pose1, 99999999.9
+
+
+def get_cam_pose(msg):
+    detected_tag = msg.detections[0]
+    dtag_pose, dtag_error = get_dtag_pose(msg)
+    reftag_pose = reftag_pose_to_matrix4(detected_tag.refTag.pose)
+    cam_pose = reftag_pose @ np.linalg.inv(dtag_pose)
+    return cam_pose, dtag_error
+
+
+def get_vio_pose(msg):
+    if hasattr(msg, 'vio'):
+        return pose_to_matrix4(msg.vio.position, msg.vio.rotation)
+    return pose_to_matrix4(msg.data.position, msg.data.rotation)
+
+
+def get_rig_pose(msg):
+    vio_pose = get_vio_pose(msg)
+    cam_pose, dtag_error = get_cam_pose(msg)
+    rig_pose = cam_pose @ np.linalg.inv(vio_pose)
+    return rig_pose, dtag_error
+
+
+if __name__ == '__main__':
+    _test_resolve_pose_ambiguity()
